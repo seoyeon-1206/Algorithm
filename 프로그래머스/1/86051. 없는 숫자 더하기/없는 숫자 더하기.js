b71b56d1cfc86d1778008d140f7a59f1c6d608c1
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-29 15:24:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,12 +1,7 @@
 function solution(numbers) {
-    var answer = 45;
-    for(let i=0; i<numbers.length; i++) {
-        for (let j=0; j<=9; j++) {
-            if (numbers[i] === j) {
-                answer-= j
-            }
-        }
+    var answer = 0;
+    for(let i=0; i<10; i++) {
+        if (!numbers.includes(i)) answer += i;
     }
-    
     return answer;
 }

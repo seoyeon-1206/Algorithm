24ab686a1519b42894236c378362ca574c9e3991
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-29 15:10:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,16 +1,20 @@
 function solution(x) {
-    var answer = true;
     x = String(x)
     var sum = 0;
-    
     for (let i=0; i<x.length; i++) {
-        sum += Number(x[i]);
-        if (Number(x) % sum === 0) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-    };
-    
-    return answer;
+        sum += Number(x[i])
+    }
+    if(Number(x) % sum === 0){
+        return true;
+    }else return false;
 }
+
+
+
+
+
+
+
+
+
+

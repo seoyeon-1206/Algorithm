1b5d64e8149295832bc323cec511db422d635d45
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-30 17:47:46 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 4.27 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,9 +1,12 @@
+// 최대공약수 : 공통 약수 중 가장 큰 수, 두 수의 곱 / 최소공배수
+// 최소공배수 : 공통된 배수 중 가장 작은 수 
+
 function solution(n, m) {
-    let min = 1;
-    for(let i=1; i<= n*m; i++){
-        if ((i % n === 0) && (i % m === 0)){
-            min = i;
-            break;
+    let min = 1
+    for (let i=1; i<=n*m; i++) {
+        if ((i % n === 0) && (i % m === 0)) {
+            min = i; // 최소공배수 
+            break; // 처음으로 찾는게 최소공배수라
         }
     }
     return[n*m/min, min];

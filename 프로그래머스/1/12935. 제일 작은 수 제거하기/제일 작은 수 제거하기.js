64ecaecde5433ea99088b05fa4405551e0a7f860
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-30 14:59:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,8 +1,5 @@
 function solution(arr) {
-    var answer = [-1];
-    if (arr.length >2) { 
-    arr.splice(arr.indexOf(Math.min(...arr)), 1)
-        return arr;
-    }
-    return answer;
+   arr.splice(arr.indexOf(Math.min(...arr)), 1)
+   arr.length <= 1 ? answer = [-1] : answer = arr
+    return answer
 }

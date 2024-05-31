a583139808b1125ac477749f2d4a88c54e503be7
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-31 15:52:27 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,14 @@
 function solution(d, budget) {
+    // 오름차순 정렬
     var answer = 0;
-    var sum = 0;
-    // 적은 금액 신청 부서부터 => 그래야 최대 =>오름차순으로 정렬
-    d = d.sort((a,b) => (a-b))
+    d = d.sort((a,b) => (a-b));
+
     for (let i=0; i<d.length; i++) {
-      if (d[i] > budget) break;
-        budget -= d[i]
-        answer ++
+        if (budget >= d[i]) {
+            budget -= d[i]
+            answer ++
         }
-    return answer;
+        
+    }
+    return answer
 }

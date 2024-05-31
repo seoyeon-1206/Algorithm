836721767b9c181dc3e861333dca1c58f11dec3c
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-05-31 15:40:07 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,8 +1,10 @@
-function solution(t, p){
-    var answer = 0
-    for (let i=0; i<=t.length - p.length; i++){
-        const str = t.substr(i,p.length)
-        if (str <= Number(p)) answer += 1       
+function solution(t, p) {
+    var answer = 0;
+    for (let i=0; i<=t.length-p.length; i++) {
+         const str = t.substr(i, p.length)
+         if (Number(str) <= Number(p)) {
+             answer ++
+        }
     }
-    return answer
+    return answer;
 }

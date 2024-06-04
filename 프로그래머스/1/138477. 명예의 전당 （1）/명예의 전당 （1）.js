7af7849f24497e2c57f9854cb580598654d04f32
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-04 14:50:14 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,12 +1,14 @@
-function solution(k, score) {
+function solution(k,score) {
+    // 명예의 전당 중 최하점만 모은 배열
     var answer = [];
+    // 명예의 전당 배열
     var honor = [];
-    score.forEach((num) => {
-        honor.push(num)
-        honor.sort((a,b) => b-a).splice(k)
     
+    for (let i=0; i<score.length; i++) {
+       // sort 후 자르기
+        honor.push(score[i]);
+        honor.sort((a,b) => (b-a)).splice(k)
         answer.push(Math.min(...honor))
-    })
-
-    return answer;
+    }
+    return answer
 }

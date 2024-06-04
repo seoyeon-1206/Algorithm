b928712542bd013f64b2d4498b19f791053e3f35
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-04 15:53:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.47 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
 
 ### 성능 요약
 
-메모리: 36.7 MB, 시간: 2.65 ms
+메모리: 36.9 MB, 시간: 2.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 08일 10:30:12
+2024년 06월 04일 15:53:34
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,19 +1,19 @@
 function solution(number, limit, power) {
     var answer = 0;
-    for(let i=1; i<=number; i++){
-        // i의 약수의 개수를 구해보자
-        let count = 0;
-        for(let j=1; j<=Math.sqrt(i); j++){
-            if(i%j === 0) {
-                count += (j*j === i) ? 1 :2; //i가 제곱수면 1을 더함
-            }
-        } 
     
-        if(count>limit){
-            answer+= power
-        }else{
-            answer += count
-        } 
+    for (let i=1; i<= number; i++) {
+        var count = 0;
+        for (let j=1; j<=Math.sqrt(i); j++) {
+        if ( i % j === 0) {
+            count += (j*j === i) ? 1 :2;
+        }
     }
-    return answer;
+        if (count >limit) {
+            answer += power
+        } else {
+            answer += count
+        }
+}
+    return answer
+    
 }

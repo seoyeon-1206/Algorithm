0a04db2987755797bf2f6a8c575a35550252f9e1
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-05 15:45:25 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -4,31 +4,17 @@ function solution(lottos, win_nums) {
     let zeroCount = 0; //0 번호의 개수
     let maxCount = 0; // 최고 순위
     let minCount = 0; // 최저 순위
+    let rank = [6,6,5,4,3,2,1]
     
-    for (let i=0; i<lottos.length; i++){
-         if(win_nums.includes(lottos[i])) {
-        //console.log(win_nums[i])
-        count ++
-        }    
-        if(lottos[i] === 0){
+    for (let i=0; i<win_nums.length; i++) {
+        if (win_nums.includes(lottos[i])) {
+            count ++
+        }
+        if (lottos[i] === 0) {
             zeroCount ++
         }
     }
-    
-    // count 가지고 result 만들기
-    if (count + zeroCount  === 0 || count + zeroCount  === 1) {
-        maxCount = 6
-    } else {
-        maxCount = 7 -(count + zeroCount)
-    }
-    
-    if (count === 0 || count === 1) {
-        minCount = 6
-    } else{
-        minCount = 7 - count
-    }
-    
-    answer = [maxCount, minCount]
-    
-    return answer;
+    maxCount = zeroCount + count;
+    minCount = count;
+    return [rank[maxCount], rank[minCount]];
 }

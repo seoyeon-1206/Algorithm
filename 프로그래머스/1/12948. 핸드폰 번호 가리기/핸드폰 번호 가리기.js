e4d11316b7a65670292e59831c2a50c2c21754a7
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-11 17:05:31 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,12 @@
-function solution (phone_number) {
-    for (let i=0; i<phone_number.length-4; i++){
-         phone_number = phone_number.replace(phone_number[i], "*")
-        console.log(phone_number)   
+function solution(phone_number) {
+    var answer = "";
+    var n = phone_number.length;
+
+    for (let i=0; i<n-4; i++) {
+        answer += '*'
     }
-    return phone_number
+    answer += phone_number.slice(n-4)
+    return answer;
 }
 
 

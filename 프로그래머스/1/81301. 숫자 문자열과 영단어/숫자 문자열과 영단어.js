25da5f1d86e0d900baa6a5a374785ba6686c7693
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-13 14:39:03 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,10 @@
 function solution(s) {
     var answer = s
     const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-   for (let i=0; i<numbers.length; i++) {
-       const arr = answer.split(numbers[i]) // split 구분자는 결과 배열에 포함되지 않는다.
-       console.log(arr) //'', '4seveneight'
-       answer = arr.join(i) // 배열을 문자열로 결합
-   }
-    return Number(answer)  
+    
+    for (let i=0; i<numbers.length; i++) {
+        let arr = answer.split(numbers[i])
+        answer = arr.join(i)
+    }
+   return Number(answer);
 }

73a8d5ff6fc8eacd1a31e50ff8a73eaf35ad679a
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-13 16:09:38 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,15 +1,12 @@
 function solution(N, stages) {
     var fail = [];
-    for (let i=1; i<=N; i++) {
-        // i의 개수
-        const notReach = stages.filter((x) => x === i).length
-        // 분모 => filter
-        const reach = stages.filter((x) => x >= i).length
-        fail.push([i, notReach/reach]);
-    }
-    // 내림차순 정렬
-    fail.sort((a,b)=> b[1] - a[1])
-     
-    const answer = fail.map((x) => x[0]);
-    return answer;
+   for (let i=1; i<=N; i++) {
+       // 분모
+       let notReach = stages.filter(num => num === i).length;
+       // 분자 스테이지에 도달한 플레이어 수
+       let reach = stages.filter(num => num >= i).length;
+       fail.push([i, notReach/ reach])
+   }
+    fail.sort((a,b) => b[1]-a[1])
+    return fail.map(x=>x[0])
 }

85e4900b86e3c86f04a8acde36b8b3b3a01f7cc1
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2024-06-13 16:26:45 +0900

    [level 1] Title: 옹알이 (2), Time: 3.83 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -6,15 +6,18 @@ function solution (babbling) {
         let babble = babbling[i]
         
         for(let j = 0; j < can.length; j++){ 
-            // 연속해서 같은 발음을 하는 것을 어려워 함
-            if(babble.includes(can[j].repeat(2))){
+            if (babble.includes(can[j].repeat(2))) {
                 break;
             }
             babble = babble.split(can[j]).join(" ")
-        } 
-        if(babble.split(" ").join("").length === 0){
-            answer += 1;
+        }
+        console.log(babble)
+        console.log(babble.split(" ").join(""))
+        // if (babble.split(" ").join("").length === 0) {
+        // answer++ }
+        if (babble.trim() === "") {
+            answer++;
         }
     }
-    return answer
+     return answer   
 }

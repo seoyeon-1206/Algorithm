2fcfc0f0617081cb47e524f0df0f4a3ded7bf65d
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2025-01-11 23:49:20 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+import sys
+K = int(sys.stdin.readline())
+arr = []
+for _ in range(K):
+    n = int(sys.stdin.readline())
+    if n == 0 :
+        arr.pop()
+    else : 
+        arr.append(n)
+print(sum(arr))
+        

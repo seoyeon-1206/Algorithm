e53ab97d62d8173a4867f28ffe1c04817e9b2539
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2025-02-16 22:24:07 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -2,28 +2,19 @@ n, k = map(int, input().split())
 female = {i: 0 for i in range(1, 7)}
 male = {i: 0 for i in range(1, 7)}
 room = 0
-for i in range(n):
+
+for _ in range(n):
     s, grade = map(int, input().split())
     if s == 0:
-        female[grade] +=1
-    if s == 1:
+        female[grade] += 1
+    else:
         male[grade] += 1
 
+# 방 개수 계산
 for value in female.values():
-    if 0 < value <= k:
-        room +=1
-    if value > k and value % 2 !=0:
-        room += value // 2 +1
-    if value > k and value & 2 == 0:
-        room += value //2
+    room += (value + k - 1) // k  # 올림 효과
 
 for value in male.values():
-    if 0 < value <= k:
-        room +=1
-    if value > k and value % 2 !=0:
-        room += value // 2 +1
-    if value > k and value & 2 == 0:
-        room += value //2
+    room += (value + k - 1) // k  # 올림 효과
 
 print(room)
-

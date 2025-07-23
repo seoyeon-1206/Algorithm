de693b17ce0d1a96ec92fe0e51ce6ca0a2836431
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2025-07-23 20:13:10 +0900

    Title: 컨베이어 벨트, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/컨베이어 벨트/conveyor-belt.py
+++ 250723/컨베이어 벨트/conveyor-belt.py
@@ -1,18 +1,9 @@
 n, t = map(int, input().split())
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
+arr = list(map(int, input().split())) + list(map(int, input().split()))
 
 for _ in range(t):
-    temp1 = arr1[-1]
-    temp2 = arr2[-1]
-    for i in range(n-1, 0, -1):
-        arr1[i] = arr1[i-1]
-        arr2[i] = arr2[i-1]
-    arr1[0] = temp2
-    arr2[0] = temp1
+    arr = [arr[-1]] + arr[:-1]
 
-print(*arr1)
-print(*arr2)
-
-
+print(*arr[:n])
+print(*arr[n:])
 

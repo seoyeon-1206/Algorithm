d7a32f02466a1dff856deb27d8c5c7597eeb2151
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2025-07-23 20:59:29 +0900

    Title: 1차원 젠가, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/1차원 젠가/jenga-1d.py
+++ 250723/1차원 젠가/jenga-1d.py
@@ -3,10 +3,19 @@ blocks = [int(input()) for _ in range(n)]
 s1, e1 = map(int, input().split())
 s2, e2 = map(int, input().split())
 
-# Please write your code here.
+def remove_blocks(s, e):
+    temp = []
+    global blocks
+    s, e = s-1, e-1
+    for i in range(len(blocks)):
+        if s<=i<=e:
+            continue
+        temp.append(blocks[i])
+    blocks = temp
+    return blocks
 
-del blocks[s1-1:e1]
-del blocks[s2-1:e2]
+remove_blocks(s1, e1)
+remove_blocks(s2, e2)
 
 print(len(blocks))
 for i in blocks:

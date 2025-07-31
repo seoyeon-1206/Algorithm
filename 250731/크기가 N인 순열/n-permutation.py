4135bddd3161783375bfbc4d33c9834d84e9b360
seoyeon-1206 <128902050+seoyeon-1206@users.noreply.github.com>
2025-07-31 16:57:08 +0900

    Title: 크기가 N인 순열, Time: 175ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 250731/크기가 N인 순열/n-permutation.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+visited = [False] * (n+1)
+answer = []
+
+def choose(curr):
+    if curr ==  n+1:
+        print(*answer)
+        return
+
+    for i in range(1, n+1):
+        if not visited[i]:
+            answer.append(i)
+            visited[i] = True
+            choose(curr + 1)
+            answer.pop()
+            visited[i] = False
+
+choose(1)
+            
